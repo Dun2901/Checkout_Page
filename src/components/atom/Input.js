8f0267d408867571c4485dabe5502e464ff07c40
KoPhaiDun <lamchidung373@gmail.com>
2023-08-21 23:44:42 +0700

    fix Button.js

--- src/components/atom/Input.js
+++ src/components/atom/Input.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const TextInput = ({ type, placeholder, label, icon }) => {
   const style = {
-    container: "mb-5 relative",
+    container: "mb-5 relative mr-3",
     label: "block mb-2",
-    input:
-      "border border-gray-600 rounded-xl py-3 pl-12 w-full tracking-1 outline-none text-base",
+    input: "border border-gray-600 rounded-xl px-3 py-3 pl-12 w-full text-base",
     iconContainer: "absolute bottom-4 left-4 text-xl",
   };
   return (
